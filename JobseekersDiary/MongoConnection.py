cee615d66ec8546f1cf766f48ba4fc5e128c7b3a
Nik Medgyesy <NikZM@Niks-MacBook-Pro.local>
2018-03-23 21:01:41 +0000

    Change data structure to embed activites to a position

--- JobseekersDiary/MongoConnection.py
+++ JobseekersDiary/MongoConnection.py
@@ -1,38 +1,55 @@
 from pymongo import MongoClient
 from bson.json_util import dumps
-from flask import json
 from bson.objectid import ObjectId
 
 import dbconfig
 
 
-def __get_activities_database():
+def __get_jsa_database():
     client = MongoClient(dbconfig.get_connection_string())
-    return client.jsa.activities
+    return client.jsa
 
 
-def add_activity(name, company, position, date, description):
-    activities = __get_activities_database()
-    result = activities.insert_one(
-        {
-            "user": name,
-            "company": company,
-            "position": position,
-            "date": date,
-            "description": description
-        })
-    return result.acknowledged
+def add_new_user(user_name):
+    users = __get_jsa_database().users
+    inserted_id = users.insert_one({"user_name": user_name}).inserted_id
+    return inserted_id
 
 
-def retrive_job_activities(name):
-    activities = __get_activities_database()
-    users_activities = activities.find({"user": name})
+def retrieve_user_id(user_name):
+    users = __get_jsa_database().users
+    user_id = users.find_one({"user_name": user_name})
+    return user_id['_id']
+
+
+def add_activity(user_id, company, position, date, description):
+    activities = __get_jsa_database().activities
+    result = activities.update_one(
+        {"user_id": user_id,
+         "company": company,
+         "position": position
+         },
+        {"$push": {
+            "activity": {
+                "date": date,
+                "description": description
+            }
+        }
+        }, upsert=True).acknowledged
+    return result
+
+
+def retrieve_job_activities(user_id):
+    activities = __get_jsa_database().activities
+    users_activities = activities.find({"user_id": user_id})
     result = dumps(users_activities)
     return result
 
 
-def retrive_job_activity(object_id):
-    activities = __get_activities_database()
+def retrieve_job_activity(object_id):
+    activities = __get_jsa_database()
     activity = activities.find_one({"_id": ObjectId(object_id)})
     result = dumps(activity)
     return result
+
+# print retrieve_job_activities("5ab55684a313fc8a5226da9f")
